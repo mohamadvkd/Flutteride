344b38f221a6b6180a64418cdb3c7cc55fc54443
Mohamad 2007 <almohamad152007@gmail.com>
2026-04-28 22:54:30 +0300

    Build from FlutterIDE - ٢٠٢٦/٠٤/٢٨ ٢٢:٥٤:٢٩

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 void main() {
   runZonedGuarded(() {
-    debugPrint('========== تطبيق FlutterIDE يبدأ ==========');
     runApp(MyApp());
   }, (error, stackTrace) {
-    debugPrint('========== خطأ غير متوقع ==========');
-    debugPrint('Error: ' + error.toString());
-    debugPrint('StackTrace: ' + stackTrace.toString());
+    try {
+      final dir = Directory('/storage/emulated/0/Download');
+      if (!dir.existsSync()) dir.createSync(recursive: true);
+      final file = File(dir.path + '/flutter_error_log.txt');
+      final log = 'ERROR: ' + error.toString() + '\n\n'
+        'STACKTRACE: ' + stackTrace.toString() + '\n\n'
+        'TIME: ' + DateTime.now().toString() + '\n';
+      file.writeAsStringSync(log);
+    } catch (_) {}
   });
 }
 
@@ -17,15 +23,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('تم بناء MyApp');
     return MaterialApp(
-      title: 'Lllll',
+      title: 'Moh',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Lllll'),
+          title: Text('Moh'),
         ),
         body: Center(
           child: Text('Hello World'),
