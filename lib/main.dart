35a8eb930e7608d3f2192c7e67cacc83bb12e8a0
Mohamad 2007 <almohamad152007@gmail.com>
2026-04-29 02:29:36 +0300

    Build from FlutterIDE - ٢٠٢٦/٠٤/٢٩ ٠٢:٢٩:٣٦

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,25 @@
-import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
 
 void main() {
-  print('===== main() بدأ =====');
-  try {
-    runApp(MyApp());
-    print('===== runApp() نجح =====');
-  } catch (e) {
-    print('===== خطأ في runApp =====');
-    print(e.toString());
-    try {
-      final dir = Directory('/data/data/Fjfj.ckck.fkckf/files');
-      if (!dir.existsSync()) dir.createSync(recursive: true);
-      File(dir.path + '/flutter_error_log.txt').writeAsStringSync(
-        'ERROR in runApp: ' + e.toString() + '\n'
-        'TIME: ' + DateTime.now().toString() + '\n'
-      );
-    } catch (_) {}
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    print('===== MyApp.build() =====');
     return MaterialApp(
-      title: 'Fkfkfkf',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Fkfkfkf'),
+          title: Text('Hello World'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.blue,
+            ),
+          ),
         ),
       ),
     );
