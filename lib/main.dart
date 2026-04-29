68df48e63216bcc1160315186e9f2c187f5a2dfc
Mohamad 2007 <almohamad152007@gmail.com>
2026-04-29 03:16:09 +0300

    Build from FlutterIDE - ٢٠٢٦/٠٤/٢٩ ٠٣:١٦:٠٨

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runZonedGuarded(() {
+    runApp(MyApp());
+  }, (error, stackTrace) {
+    try {
+      final dir = Directory('/storage/emulated/0/Download');
+      if (!dir.existsSync()) dir.createSync(recursive: true);
+      final file = File(dir.path + '/flutter_error_log.txt');
+      final log = 'ERROR: ' + error.toString() + '\n\n'
+        'STACKTRACE: ' + stackTrace.toString() + '\n\n'
+        'TIME: ' + DateTime.now().toString() + '\n';
+      file.writeAsStringSync(log);
+    } catch (_) {}
+  });
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Moh',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World'),
+          title: Text('Moh'),
         ),
         body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.blue,
-            ),
-          ),
+          child: Text('Hello World'),
         ),
       ),
     );
