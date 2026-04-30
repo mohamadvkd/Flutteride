527833207011d31ca5041cccd714c41a3b5c5822
Mohamad 2007 <almohamad152007@gmail.com>
2026-04-30 19:01:27 +0300

    Build from FlutterIDE - 2026-04-30T16:01:27.147Z

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Qq',vn.,,ccb
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBannerب: false,
       theme: ThemeData.dark().copyWith(
         scaffo{{{{{{{gg()}}}}}}}ldBackgroundColor: const Color(0xFF1A1A2E),
       ),
